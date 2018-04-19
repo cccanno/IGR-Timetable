fd80034b28c6d2cea1e6365fe8c7fc28e7b7557e
canno <canno@monto.works>
2018-04-19 14:24:55 +0900

    version up & deleted map

--- app/alloy.js
+++ app/alloy.js
@@ -1,17 +1,5 @@
-// The contents of this file will be executed before any of
-// your view controllers are ever executed, including the index.
-// You have access to all functionality on the `Alloy` namespace.
-//
-// This is a great place to do any initialization for your app
-// or create any global variables/functions that you'd like to
-// make available throughout your app. You can easily make things
-// accessible globally by attaching them to the `Alloy.Globals`
-// object. For example:
-//
-// Alloy.Globals.someGlobalFunction = function(){};
-
 Alloy.Globals.currentPage = 0;
-Alloy.Globals.Map = require('ti.map');
+// Alloy.Globals.Map = require('ti.map');
 Alloy.Globals.latitude = 39.702053;
 Alloy.Globals.longitude = 141.154484;
 Alloy.Globals.halfDisplayWidth = Ti.Platform.displayCaps.platformWidth * 0.5;
